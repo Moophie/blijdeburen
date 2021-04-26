b7b0eab72b5587729c378e12ce188cdfcfbee7ef
Moophie <michael.van.lierde@hotmail.com>
2021-04-26 15:44:17 +0200

    dev: add seeders for things and services

--- database/migrations/2021_04_26_131805_create_services_table.php
+++ database/migrations/2021_04_26_131805_create_services_table.php
@@ -17,6 +17,7 @@ class CreateServicesTable extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
+            $table->float('price');
             $table->integer('user_id');
             $table->timestamps();
         });
