1d6f081f2bde9d28eac29d7ad03048dffb0e2754
Moophie <michael.van.lierde@hotmail.com>
2021-06-03 18:34:02 +0200

    comment: fixed footer example

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,19 @@
 <nav>
     <a href="/"><img src="/images/icons/icon_home.svg" width="35px" alt="">Home</a>
-    @if(Auth::user())
-    <a href="/transactions"><img src="/images/icons/icon_transactions.svg" width="35px" alt="">Transacties</a>
-    <a href="/offer"><img src="/images/icons/icon_offer.svg" width="35px" alt="">Aanbieden</a>
+    @if (Auth::user())
+        <a href="/transactions"><img src="/images/icons/icon_transactions.svg" width="35px" alt="">Transacties</a>
+        <a href="/offer"><img src="/images/icons/icon_offer.svg" width="35px" alt="">Aanbieden</a>
     @endif
     <a href="/profile"><img src="/images/icons/icon_profile.svg" width="35px" alt="">Profiel</a>
 </nav>
+{{-- <style>
+    nav {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: red;
+        color: white;
+        text-align: center;
+    }
+</style> --}}
