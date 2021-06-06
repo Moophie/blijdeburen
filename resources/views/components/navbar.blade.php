c948932631e572dc3e3d9427955928c802c74dca
Miyaa Kan <marina.kan@hotmail.com>
2021-06-06 19:08:30 +0200

    nav edit

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,8 @@
 <nav>
-    <a href="/"><img src="/images/icons/icon_home.svg" width="35px" alt="">Home</a>
+    <a href="/"><img src="/images/icons/icon_home.svg" width="35px" alt=""><p>Home</p></a>
     @if (Auth::user())
-        <a href="/transactions/Actief"><img src="/images/icons/icon_transactions.svg" width="35px" alt="">Transacties</a>
-        <a href="/offer"><img src="/images/icons/icon_offer.svg" width="35px" alt="">Aanbieden</a>
+        <a href="/transactions/Actief"><img src="/images/icons/icon_transactions.svg" width="35px" alt=""><p>Transacties</p></a>
+        <a href="/offer"><img src="/images/icons/icon_offer.svg" width="35px" alt=""><p>Aanbieden</p></a>
     @endif
-    <a href="/profile"><img src="/images/icons/icon_profile.svg" width="35px" alt="">Profiel</a>
+    <a href="/profile"><img src="/images/icons/icon_profile.svg" width="35px" alt=""><p>Profiel</p></a>
 </nav>
-{{-- <style>
-    nav {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: red;
-        color: white;
-        text-align: center;
-    }
-</style> --}}
